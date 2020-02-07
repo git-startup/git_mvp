99a037becd2689a91971ffff239be8edae095ee4
git-startup <gitstartup.sd@gmail.com>
2020-02-07 01:12:01 -0800

    change style.css file

--- public/js/app.js
+++ public/js/app.js
@@ -85860,8 +85860,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\blog\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\blog\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\git_startup_mvp\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\wamp64\www\git_startup_mvp\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
